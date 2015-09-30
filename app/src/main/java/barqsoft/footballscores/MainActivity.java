394a125d975bc381642d6fca7b51cb472c4340a6
kyleparker <kyleparker@bsu.edu>
2015-09-30 07:48:59 -0400

    Code cleanup

--- app/src/main/java/barqsoft/footballscores/MainActivity.java
+++ app/src/main/java/barqsoft/footballscores/MainActivity.java
@@ -96,8 +96,6 @@ public class MainActivity extends BaseActivity {
     private static final TypeEvaluator<?> ARGB_EVALUATOR = new ArgbEvaluator();
     private ObjectAnimator mStatusBarColorAnimator;
 
-//    private PagerFragment my_main;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
